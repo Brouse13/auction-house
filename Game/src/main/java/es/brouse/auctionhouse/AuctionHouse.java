59e92135aad05b204268d74fb1ec12eb117de62b
Brouse_13 <unaiunai2004@gmail.com>
2022-07-30 23:34:51 +0200

    [FIX] Now all the handlers initialized correctly

--- Game/src/main/java/es/brouse/auctionhouse/AuctionHouse.java
+++ Game/src/main/java/es/brouse/auctionhouse/AuctionHouse.java
@@ -1,14 +1,30 @@
 package es.brouse.auctionhouse;
 
+import es.brouse.auctionhouse.commands.AHCommand;
 import es.brouse.auctionhouse.loader.commands.CommandRegister;
+import es.brouse.auctionhouse.loader.config.Config;
+import es.brouse.auctionhouse.loader.config.MysqlConfig;
+import es.brouse.auctionhouse.loader.config.YamlConfig;
+import es.brouse.auctionhouse.loader.storage.YAML;
 import es.brouse.auctionhouse.loader.utils.Logger;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuctionHouse extends JavaPlugin {
+    @Getter private Config settings;
+
     @Override
     public void onEnable() {
+        //Init utils
         Logger.init(this);
+        YAML.init(this);
         CommandRegister.init(this);
+
+        //SetUp mysql if possible
+        settings = new YamlConfig();
+        if (settings.isMysqlEnabled()) settings = new MysqlConfig(settings);
+
+        CommandRegister.register(new AHCommand());
     }
 
     @Override
