0a611eded0d22265d7a14d56e44fa386ac19c957
Brouse_13 <unaiunai2004@gmail.com>
2022-08-01 21:34:53 +0200

    [FIX] Now can get settings statically

--- Game/src/main/java/es/brouse/auctionhouse/AuctionHouse.java
+++ Game/src/main/java/es/brouse/auctionhouse/AuctionHouse.java
@@ -5,13 +5,13 @@ import es.brouse.auctionhouse.loader.commands.CommandRegister;
 import es.brouse.auctionhouse.loader.config.Config;
 import es.brouse.auctionhouse.loader.config.MysqlConfig;
 import es.brouse.auctionhouse.loader.config.YamlConfig;
+import es.brouse.auctionhouse.loader.inventory.InventoryController;
 import es.brouse.auctionhouse.loader.storage.YAML;
 import es.brouse.auctionhouse.loader.utils.Logger;
-import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuctionHouse extends JavaPlugin {
-    @Getter private Config settings;
+    private static Config settings;
 
     @Override
     public void onEnable() {
@@ -24,11 +24,17 @@ public class AuctionHouse extends JavaPlugin {
         settings = new YamlConfig();
         if (settings.isMysqlEnabled()) settings = new MysqlConfig(settings);
 
+        this.getServer().getPluginManager().registerEvents(new InventoryController(), this);
+
         CommandRegister.register(new AHCommand());
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public static Config getSettings() {
+        return settings;
     }
 }
